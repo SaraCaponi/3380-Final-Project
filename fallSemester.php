8cfa6aa856c4df215f69c2a8ba9eb8b52c4a3086
Sara Caponi <sbc436@mail.missouri.edu>
2018-04-24 13:30:25 -0500

    displayed data base info on this page

--- fallSemester.php
+++ fallSemester.php
@@ -1,13 +1,59 @@
+<?php
+
+$servername = "sql207.epizy.com";
+$username = "epiz_21498761";
+$password = "kViPIdVw5uif";
+$dbname = "epiz_21498761_MizzouCalendar";
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("<p>Connection failed: " . $conn->connect_error . "</p>");
+}
+
+$sql = "SELECT * FROM fall";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+
+	$events = array();
+	while($row = $result->fetch_assoc()) {
+        array_push($events, $row);
+    }
+    
+    if ($_GET["responseType"] == "json") {
+    	print json_encode($events);
+    } else {  // html page
+    	print generatePageHTML("Events", generateTaskTableHTML($events));
+    }
+}
+
+function generateTaskTableHTML($events) {
+	$html = "<table>\n";
+	$html .= "<tr><th>Organization</th><th>Event</th><th>Description</th><th>Location</th><th>Time</th></tr>\n";
+	
+	foreach ($events as $event) {
+		$html .= "<tr><td>{$event['organization']}</td><td>{$event['event']}</td><td>{$event['description']}</td><td>{$event['location']}</td><td>{$event['whatTime']}</td></tr>\n";
+	}
+	$html .= "</table>\n";
+	
+	return $html;
+}
+
+function generatePageHTML($title, $body) {
+	$html = <<<EOT
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Fall Semester</title>
-	<meta charset="utf-8">
+<style> body{ background: red;} </style>
+<title>$title</title>
 </head>
 <body>
-	<h1>Fall Semester Activities</h1>
-	
-	
-</body>	
-	
+$body
+</body>
 </html>
+EOT;
+
+	return $html;
+}
+?>
