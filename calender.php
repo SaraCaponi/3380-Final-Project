c1516eff741cf5fb1c1126a0b53e633aeeea5960
Kate Gardner <KateGardner@mwc-065243.dhcp.missouri.edu>
2018-04-30 16:06:59 -0500

    styling

--- calender.php
+++ calender.php
@@ -4,35 +4,39 @@
     $version = $_GET['ver'];
 ?>
 h1{
-font-family: 'Oxygen', sans-serif;
- font-size: 70px;
-text-align: center;
-position: absolute;
-left: 50%;
-top: 90%;
-}
+    font-family: 'Oxygen', sans-serif;
+    font-size: 70px;
+    text-align: center;
+    position: absolute;
+    left: 50%;
+    top: 90%;
+    }
 body{
 	background-color: lightgrey;
 }
 table, th, td{
-border: 2px solid black;
-text-align: center;
-padding: 2px;
+    border: 2px solid black;
+    text-align: center;
+    padding: 2px;
 }
 #rows{
-font-size: 40px;
-font-family: 'Quicksand', sans-serif;
-background-color: gold;
+    font-size: 40px;
+    font-family: 'Quicksand', sans-serif;
+    background-color: gold;
 }
 #info{
-font-size:20px;
-font-family: 'Quicksand', sans-serif;
+    font-size:20px;
+    font-family: 'Quicksand', sans-serif;
 }
 .banner{
-width: 100%;
-display: block;
+    width: 100%;
+    display: block;
 }
-.banner-image{
-width: 100%;
-display: block;
+.banner > .banner-image{
+    width: 100%;
+    display: block;
+}
+.nav{
+    text-align: right;
+    padding: 10px 0 10 0;
 }
--- springSemester.php
+++ springSemester.php
@@ -1,21 +1,17 @@
-<?php
-
+<?php 
+session_start();
 $servername = "sql207.epizy.com";
 $username = "epiz_21498761";
 $password = "kViPIdVw5uif";
 $dbname = "epiz_21498761_MizzouCalendar";
 $conn = new mysqli($servername, $username, $password, $dbname);
-
 // Check connection
 if ($conn->connect_error) {
     die("<p>Connection failed: " . $conn->connect_error . "</p>");
 }
-
-$sql = "SELECT * FROM spring";
+$sql = "SELECT * FROM Events";
 $result = $conn->query($sql);
-
 if ($result->num_rows > 0) {
-
 	$events = array();
 	while($row = $result->fetch_assoc()) {
         array_push($events, $row);
@@ -24,38 +20,66 @@ if ($result->num_rows > 0) {
     if ($_GET["responseType"] == "json") {
     	print json_encode($events);
     } else {  // html page
-    	print generatePageHTML("Events", generateTaskTableHTML($events));
+    	print generatePageHTML("Events", generateEventTableHTML($events));
     }
 }
-
-function generateTaskTableHTML($events) {
-	$html = "<table>\n";
-	$html .= "<tr><th>Organization</th><th>Event</th><th>Description</th><th>Location</th><th>Time</th></tr>\n";
-	
-	foreach ($events as $event) {
-		$html .= "<tr><td>{$event['organization']}</td><td>{$event['event']}</td><td>{$event['description']}</td><td>{$event['location']}</td><td>{$event['whatTime']}</td></tr>\n";
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+	$user_id=$_SESSION['userid'];
+		 $sql2="UPDATE users SET loggedIn='N' WHERE id='$user_id'";
+		  mysqli_query($conn,$sql2);
+$URL="http://saracaponi.epizy.com/FinalProject.php";
+echo "<script type='text/javascript'>document.location.href='{$URL}';</script>";
+echo '<META HTTP-EQUIV="refresh" content="0;URL=' . $URL . '">';		
 	}
-	$html .= "</table>\n";
-	
-	return $html;
+if($_SERVER["REQUEST_METHOD"] == "GET") {
+	$user_id=$_SESSION['userid'];
+	$event_id=$_GET['id'];
+	$sql="INSERT INTO rsvpEvent (userID, eventID) VALUES('$user_id','$event_id')";
+	mysqli_query($conn,$sql);
+	echo $user_id;
+	echo $event_id;
 }
-
+function generateEventTableHTML($events) {
+	
+		$html .= "<table>\n";
+		$html .= "<tr><th>Organization</th><th>Event</th><th>Description</th><th>Location</th><th>Time</th><th>actions</th></tr>\n";
+	
+		foreach ($events as $event) {
+			$id = $event['id'];
+			$org = $event['organization'];
+			$EventTitle = $event['event'];
+			$description = ($event['description']) ? $event['description'] : '';
+			$location = $event['location'];
+			$whatTime=$event['whatTime'];
+			
+			$html .= "<tr><td>$org</td><td>$EventTitle</td><td>$description</td><td>$location</td><td>$whatTime</td><td><form action='fallSemester.php' method='get'><button type='submit' name='id' value='$id'> Add to my Events </button>
+			</form></td></tr>\n";
+		}
+		$html .= "</table>\n";
+	
+		return $html;
+}
 function generatePageHTML($title, $body) {
 	$html = <<<EOT
 <!DOCTYPE html>
 <html>
-<head>
-<link rel="stylesheet" type="text/css" href="calender.css">
+    <head>
+        <link rel="stylesheet" type="text/css" href="calender.php">
+        <link href="https://fonts.googleapis.com/css?family=Crimson+Text|Oxygen|Quicksand" rel="stylesheet">
 <title>$title</title>
 </head>
 <body>
-<button onclick="location.href='FinalProject.html'"> Home </button>
-<h1>Spring Events</h1>
+    <button onclick="location.href='myEvents.php'">View My Events</button>
+    <form action = '' method = 'post''><button type="submit"> Log Out </button>
+    
+    <div class='banner'>
+        <img class='banner-image' src="mizzou.jpg" alt="mizzou" style="width:100%; height:50%;">
+        <h1>Upcoming Events!</h1>
+    </div>
 $body
 </body>
 </html>
 EOT;
-
 	return $html;
 }
 ?>
